8626e5d5d7dd131af9e6795584913605549160fe
SAYEM5522 <md1040582@gmail.com>
2022-06-04 16:37:51 +0600

    Graph Date Select Add

--- apps/next/pages/CryptoFeed/CryptoGraph.tsx
+++ apps/next/pages/CryptoFeed/CryptoGraph.tsx
@@ -5,6 +5,7 @@ import {
 import { Line } from 'react-chartjs-2';
 import { useSelector } from 'react-redux';
 import { selectCountryName, selectTokenId, selectTokenName } from '../features/Analytics';
+import styles from "../Css/CryptoFeed/CryptoGraph.module.css"
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -14,12 +15,41 @@ ChartJS.register(
   Tooltip,
   Legend
 );
+const dataSelect=[
+  {
+    id:1,
+    type:'1H',
+    value:0.4
+  },
+  {
+    id:2,
+    type:'24H',
+    value:1
+  },
+  {
+    id:3,
+    type:'7D',
+    value:7
+  },
+  {
+    id:4,
+    type:'1M',
+    value:30
+  },
+  {
+    id:5,
+    type:'1Y',
+    value:365
+  }
+]
 const CryptoGraph = () => {
   const Token=useSelector(selectTokenId)
   const moneyInitial=useSelector(selectCountryName)
   const [Data,setData]=useState<any>([])
+  const[selectDate,setSelectDate]=useState(0.4)
+  const[currentIndex,setCurrentIndex]=useState(0)
   const getData=async()=>{
-      await  axios.get(`https://api.coingecko.com/api/v3/coins/${Token}/market_chart?vs_currency=${moneyInitial}&days=${7}`).then(res=>{
+      await  axios.get(`https://api.coingecko.com/api/v3/coins/${Token}/market_chart?vs_currency=${moneyInitial}&days=${selectDate}`).then(res=>{
          setData(res.data.prices)
         }).catch
         (err=>console.log(err))
@@ -27,7 +57,7 @@ const CryptoGraph = () => {
   useEffect(()=>{
       getData(),
       ()=>getData()
-  },[Token,moneyInitial])
+  },[Token,moneyInitial,selectDate])
   const label=Data.map((item:any)=>item[0])
   const value=Data.map((item:any)=>item[1])
 
@@ -63,7 +93,22 @@ const CryptoGraph = () => {
    
   };
   return (
-    <div>
+    <div className={styles.CryptoGraph} >
+      <div className={styles.CryptoGraph_Header}>
+      {
+        dataSelect.map((value,index)=>{
+          const Select=()=>{
+            setSelectDate(value.value),
+            setCurrentIndex(index)
+          }
+          return(
+            <div onClick={Select} className={styles.CryptoGraph_List} key={index}>
+              <p style={{"color":(currentIndex===index)?"blue":"white"}}>{value.type}</p>
+            </div>
+          )
+        })
+      }
+      </div>
     <Line options={options}  data={item} />
     </div>
   )
