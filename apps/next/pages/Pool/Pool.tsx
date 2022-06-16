0c17ea3553b2ff6dcf8a4f05757687a6ade53c2a
SAYEM5522 <md1040582@gmail.com>
2022-06-16 20:34:30 +0600

    Pool Structure Add

--- apps/next/pages/Pool/Pool.tsx
+++ apps/next/pages/Pool/Pool.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import CryptoFeed from '../CryptoFeed/CryptoFeed'
+import styles from "../Css/PoolCss/Pool.module.css"
+import Sidebar from '../Sidebar/Sidebar'
 const Pool = () => {
   return (
-    <div>Pool</div>
+    <div>
+      <div className={styles.Home_container}>
+      <Sidebar/>
+      <CryptoFeed/>
+      </div>
+    </div>
   )
 }
 
