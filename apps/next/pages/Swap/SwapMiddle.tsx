2575ac332e90f87f66e77612062f129a3d6b3122
SAYEM5522 <md1040582@gmail.com>
2022-07-03 18:47:46 +0600

    swapItem design done

--- apps/next/pages/Swap/SwapMiddle.tsx
+++ apps/next/pages/Swap/SwapMiddle.tsx
@@ -1,24 +1,62 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import styles from "../Css/SwapCss/SwapMiddle.module.css"
 import {FiSettings} from "react-icons/fi"
 import { WalletConnect } from '../WalletConnect/WalletConnect'
 const SwapMiddle = () => {
+  const [Amount1,setAmount1]=useState(0)
+  const [Amount2,setAmount2]=useState(0)
+  const [open1,setOpen1]=useState(false)
+
+  const SwapAmount1=useCallback((e)=>{
+    setAmount1(e.target.value)
+  },[])
+  const SwapAmount2=useCallback((e)=>{
+    setAmount2(e.target.value)
+  },[])
+   const TokenView1=useCallback(()=>{
+    setOpen1(!open1)
+   },[open1])
+
+
   return (
     <div style={{flex:"0.78"}}>
       <div>
-      <div className={styles.SwapMiddleFeed}>
+      <div className={styles.SwapMiddleFeed} >
         <div className={styles.SwapMiddleFeed_Header}>
          <p>Swap</p>
          <FiSettings size={27} color="#fff"/>
         </div>
         <div className={styles.SwapMiddleFeed_Body}>
-
+          <div style={{position:"relative"}}>
+         <input type="number" className={styles.Input1}  placeholder="0.0" onChange={SwapAmount1} />
+         <div className={styles.SelectToke1} onClick={TokenView1}>
+          <p>Select A Token</p>
         </div>
+        </div>
+        <div style={{position:"relative"}} >
+        <input type="number" className={styles.Input2}  placeholder="0.0" onChange={SwapAmount2}/>
+        <div className={styles.SelectToke2} onClick={TokenView1}>
+          <p>Select A Token</p>
+        </div>
+        </div>
+        </div>
+
         <div className={styles.SwapMiddleFeed_Bottom}>
           <WalletConnect/>
         </div>
+        {
+        open1?
+        <div className={styles.TokenList}>
+          <div className={styles.TokenListHeader}>
+           <p>Select Token</p>
+            <p onClick={()=>{setOpen1(false)}}>X</p>
+          </div>
+        </div>
+        :null
+      }
       </div>
       </div>
+    
     </div>
   )
 }
